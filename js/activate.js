9bd4b17a8e8166b613de83f24d200b47d85e070a
yuzhe <yuzhe.hao.cn@gmail.com>
2021-01-02 21:56:05 +0900

    first version
    could play the video now

--- js/activate.js
+++ js/activate.js
@@ -3,6 +3,7 @@ function activate(RecordName) {
     var bv = new Bideo();
     var videox = document.querySelector('#background_video');
     var bodyx = document.querySelector('body');
+    // var coverx = document.querySelector('#video_cover').
     if ($("#background_video")[0].childElementCount > 0) {
         $("#background_video")[0].removeChild($("#background_video")[0].firstChild)
     };
@@ -28,13 +29,40 @@ function activate(RecordName) {
         // Array of objects containing the src and type
         // of different video formats to add
         src: [{
-            src: "/Users/Yuzhe/Music/Monster\ Siren\ Records/" + RecordName + ".mp4",
+            // src: "/Users/Yuzhe/Music/Monster\ Siren\ Records/" + RecordName + ".mp4",
+
+            src: "../resource/" + RecordName + ".mp4",
             type: 'video/mp4'
         }],
 
-        // What to do once video loads (initial frame)
+        // What to do once video loads(initial frame)
         // onLoad: function() {
-        //     document.querySelector('#video_cover').style.display = 'none';
+        //     coverx.style.display = 'none';
         // }
     });
 };
+
+var No001 = "SPEED OF LIGHT";
+var No002 = "Boiling Blood";
+var No003 = "Renegade";
+var No004 = "Sparkling Hydraulics";
+var No005 = "El Brillo Solitario";
+var No006 = "Evolutionary Mechanization";
+var No007 = "Reconnection";
+var No008 = "Confront";
+var No009 = "Lithos";
+
+
+// <p>💿···</p>
+
+function switch_to(RecordCode) {
+    for (var i = $("div.record").length - 1; i >= 0; i--) {
+        if ($("div.record")[i].childElementCount > 0) {
+            $("div.record")[i].removeChild($("div.record")[i].firstChild)
+        };
+    }
+    $("#" + RecordCode)[0].innerHTML = "<p>💿··· </p>" + $("#" + RecordCode)[0].innerHTML
+
+    RecordName = eval(RecordCode)
+    activate(RecordName)
+}
