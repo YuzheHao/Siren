8ae9e1e6bddcbb74ef843273d0bd96c415864b0d
yuzhe <yuzhe.hao.cn@gmail.com>
2021-01-03 23:19:58 +0900

    version 2.0
    easy to use, freely add new reocrds
    and add a cover for the page

--- js/activate.js
+++ js/activate.js
@@ -3,9 +3,10 @@ function activate(RecordName) {
     var bv = new Bideo();
     var videox = document.querySelector('#background_video');
     var bodyx = document.querySelector('body');
-    var playx = document.querySelector('#play');
-    var pausex = document.querySelector('#pause');
-    // var coverx = document.querySelector('#video_cover').
+    var coverx = document.querySelector('#video_cover');
+
+    // var playx = document.querySelector('#play');
+    // var pausex = document.querySelector('#pause');
     if ($("#background_video")[0].childElementCount > 0) {
         $("#background_video")[0].removeChild($("#background_video")[0].firstChild)
     };
@@ -25,14 +26,13 @@ function activate(RecordName) {
 
         isMobile: window.matchMedia('(max-width: 768px)').matches,
 
-        playButton: playx,
-        pauseButton: pausex,
+        // playButton: playx,
+        // pauseButton: pausex,
 
         // Array of objects containing the src and type
         // of different video formats to add
         src: [{
             src: "/Users/Yuzhe/Music/Monster\ Siren\ Records/" + RecordName + ".mp4",
-            // src: "../resource/" + RecordName + ".mp4",
             type: 'video/mp4'
         }],
 
@@ -49,9 +49,16 @@ function switch_to(RecordCode) {
             $("div.record")[i].removeChild($("div.record")[i].firstChild)
         };
     }
-
     activate($("#" + RecordCode)[0].textContent)
     $("#" + RecordCode)[0].innerHTML = "<p>💿&nbsp</p>" + $("#" + RecordCode)[0].innerHTML
+}
 
-
+function homing() {
+    for (var i = $("div.record").length - 1; i >= 0; i--) {
+        if ($("div.record")[i].childElementCount > 0) {
+            $("div.record")[i].removeChild($("div.record")[i].firstChild)
+        };
+    }
+    $("#container")[0].removeChild($("#container")[0].children[1]);
+    $("#container").append("<video id='background_video' loop></video>");
 }
